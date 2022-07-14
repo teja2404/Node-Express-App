9d496e9a4ab1a0819ba80834ef7dbf93deaa6850
Arun Muthe <arun.muthe@ibm.com>
2022-07-14 13:15:01 +0530

    express-validator, inbuilt, custom, input error handling

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,7 +20,7 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
-      res.render('shop/product-detail', {
+      res.render('shop/product-details', {
         product: product,
         pageTitle: product.title,
         path: '/products'
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,69 @@
 const express = require('express')
-
+const {
+  check,
+  body,
+  validationResult
+} = require('express-validator/check')
 const authController = require('../controllers/auth')
 const router = express.Router()
 
 
 router.get('/login', authController.getLogin)
 router.get('/signup', authController.getSignup);
-router.post('/login', authController.postLogin)
-router.post('/signup', authController.postSignup);
+router.post(
+  '/login',
+  [
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email address.')
+    .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+    .isLength({
+      min: 5
+    })
+    .isAlphanumeric()
+    .trim()
+  ],
+  authController.postLogin
+);
+router.post('/signup', [check('email').isEmail().withMessage('Please enter a valid email').custom((value, {
+    req
+  }) => {
+
+    // if (value == 'test@test.com') {
+    //   throw new Error("this email is address is forbidden")
+    // }
+    // return true
+    return User.findOne({
+      email: value
+    }).then(userDoc => {
+      if (userDoc) {
+        return Promise.reject(
+          'E-Mail exists already, please pick a different one.'
+        );
+      }
+    });
+
+  })
+  .normalizeEmail(),
+  body('password', 'Please Enter a Password with only numbers and text , and at least 5 characters.').isLength({
+    min: 5
+  }).isAlphanumeric()
+  .trim(),
+  body('confirmPassword').custom((value, {
+    req
+  }) => {
+    if (value !== req.body.password) {
+      throw new Error("password as  to match with confirm password")
+    }
+    return true
+  })
+  .trim()
+], authController.postSignup);
 router.post('/logout', authController.postLogout)
+router.get('/reset', authController.getReset)
+router.post('/reset', authController.postReset)
+router.get('/reset/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router
